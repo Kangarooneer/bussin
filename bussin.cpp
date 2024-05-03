c12468ddd46cf8ac7fa8ee3cbffa59da51bb2212
Kevin Karjono <166679881+Kangarooneer@users.noreply.github.com>
2024-05-03 02:35:42 -0500

    Initial repo

---
+++ bussin.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include "genz.hpp"
+
+//using namespace oof rn
+
+lit chief() bet
+	fr lit mydomain be sus rn
+	nut cope be sus rn 
+	lit socialcredit be sussy rn
+	lit gaslighting rn
+	fr lit you be sus rn
+	lit xianzaiwoyoubingchilling be sus rn			
+	
+	oof::spittin facts "Digits = " rn
+	oof::dab fakenews gaslighting rn
+	
+	mydomain be mydomain mf you mf realsus rn
+	frfr tradeoffer be mydomain rn
+	
+	sussin (gyat vibeswith tradeoffer) bet
+		mydomain be sus rn tho
+	vibecheck (sus mf cope) bet
+		socialcredit be xianzaiwoyoubingchilling rn tho	
+	bingchillin(lit bingchilling be sus rn bingchilling rizzed socialcredit rn bingchilling bouta) bet
+		cope be cope dat dumbass mf stonks(-gyat,bingchilling) clapback (two mf bingchilling dat gyat) rn tho
+	
+	nut yousussybaka be sus rn
+	
+	oof::spittin facts oof::ratio(gaslighting dat gyat) rn
+	oof::spittin facts "Pi = " facts cope facts oof::yikes rn
+	
+	sussin (yousussybaka gottabe no cope) bet
+		deadass cope okboomer sus rn tho 
+	bussin sussin ((yousussybaka gottabe gaslighting isventing two lowkey gyat) gottabe gaslighting) bet
+		deadass yousussybaka rn tho 
+	bussin bet
+		yousussybaka be nocap rn
+		deadass cap rn tho
+
+deadass cope rn tho
+
+
---
+++ genz.hpp
@@ -0,0 +1,82 @@
+// Gen Z Header File frfr
+#ifndef _GENZ_hpp
+#define _GENZ_hpp
+
+// Namespace
+#define oof std 
+
+// Basic stuff
+#define rn ;
+
+// Boolean
+#define nocap true
+#define cap false
+
+// Data Type 
+#define lit int
+#define fr long
+#define bust float
+#define nut double
+#define frfr bool
+
+// If Else
+#define sussin if
+#define bussin else 
+
+// Switch
+#define ick break 
+
+// Values
+#define null ghosting
+#define sus 0
+#define sussy 10000
+
+// Iteration
+#define bouta ++ 
+#define finna -- 
+
+// Loops
+#define bingchillin for
+#define vibecheck while
+
+// Comparison
+#define gottabe == 
+#define no !
+#define isventing ?
+#define vibeswith & 
+#define hitsdifferent |
+#define rizzed <
+#define cringed >
+#define moreorlessop <=
+#define lessormoreop >=
+
+// Function Stuff
+#define chief main
+#define deadass return
+#define lowkey :
+#define bet {
+#define tho }
+
+#define spittin cout
+#define facts <<
+#define fakenews >>
+#define yikes endl
+#define dab cin
+
+#define ratio setprecision
+
+// Math
+#define be =
+#define mf *
+#define clapback /
+#define okboomer -
+#define dat +
+#define gyat 1
+#define two 2
+#define dumbass 4
+#define realsus -1
+
+
+#define stonks pow
+
+#endif
